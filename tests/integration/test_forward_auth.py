caa15a1de769a2ac99cf6771e01223cc311e3ed5
natalia <natalianowakowska@mail.com>
2024-07-23 14:36:52 +0200

    tests: increase retry attempts

--- tests/integration/test_forward_auth.py
+++ tests/integration/test_forward_auth.py
@@ -76,8 +76,8 @@ async def test_deployment(ops_test: OpsTest, traefik_charm, forward_auth_tester_
 
 
 @retry(
-    wait=wait_exponential(multiplier=3, min=1, max=20),
-    stop=stop_after_attempt(20),
+    wait=wait_exponential(multiplier=3, min=1, max=30),
+    stop=stop_after_attempt(30),
     reraise=True,
 )
 async def test_allowed_forward_auth_url_redirect(ops_test: OpsTest) -> None:
